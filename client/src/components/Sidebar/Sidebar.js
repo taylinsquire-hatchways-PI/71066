3186baa07b8bdcce7f6cdbeb680553b7ebd80952
Taylin Squire <taylinsquire@gmail.com>
2022-01-07 00:25:54 -0700

    Pass user to Chat component

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles(() => ({
 const Sidebar = (props) => {
   const classes = useStyles();
   const conversations = props.conversations || [];
-  const { handleChange, searchTerm } = props;
+  const { handleChange, searchTerm, user } = props;
 
   return (
     <Box className={classes.root}>
@@ -32,7 +32,7 @@ const Sidebar = (props) => {
       {conversations
         .filter((conversation) => conversation.otherUser.username.includes(searchTerm))
         .map((conversation) => {
-          return <Chat conversation={conversation} key={conversation.otherUser.username} />;
+          return <Chat conversation={conversation} key={conversation.otherUser.username} user={user} />;
         })}
     </Box>
   );
@@ -40,6 +40,7 @@ const Sidebar = (props) => {
 
 const mapStateToProps = (state) => {
   return {
+    user: state.user,
     conversations: state.conversations
   };
 };
