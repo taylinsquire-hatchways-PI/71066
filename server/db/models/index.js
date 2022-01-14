bb7bffe64fd1492183b581761a3e6bc43f25d9bc
Taylin Squire <taylinsquire@gmail.com>
2022-01-13 21:34:56 -0700

    Updated associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,7 +4,7 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
+User.belongsToMany(Conversation, { through: "Convo_Users" });
 Conversation.belongsToMany(User, { through: "Convo_Users" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
